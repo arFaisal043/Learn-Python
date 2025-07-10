a27f420e8fdff3f8658f7fac2cb4bc51ba1f99bf
arFaisal043 <arfaisal463@gmail.com>
2025-07-10 15:05:20 +0600

    complete basic syntax

---
+++ basic.py
@@ -0,0 +1,47 @@
+# Variable: 
+
+"""
+a = 4
+A = "Salary"
+PI = 3.1216
+isNotBool = "True"
+isBool = True
+print(type(isBool))
+"""
+
+
+# Type Casting:
+a = int("2")
+b = 3.25
+#print(a+b)
+#print(type(a))
+
+a = 3.1416
+a = str(a)
+#print(type(a))
+
+
+# Take input:
+"""
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+Id = int(input("Enter your ID: "))
+
+print("Name: ", name)
+print("Age: ", age)
+print("Id: ", Id)
+print(name , age , Id)
+
+
+val1 = int(input("Value 1: "))
+val2 = int(input("Value 2: "))
+
+def sum(val1 , val2):
+    return (val1 + val2) / 2
+
+print(sum(val1 , val2))
+"""
+
+# Random Number:
+import random
+#print(random.randrange(1 , 5));
